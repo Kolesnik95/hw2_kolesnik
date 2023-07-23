10dd2bf214a11b8986dfb4c9689a0afc05738c2c
Kolesnik95 <95kolesnik@gmail.com>
2023-07-23 21:52:56 +0300

    first commit

---
+++ hw2.js
@@ -0,0 +1,33 @@
+
+let name = prompt('Як вас звати?');
+alert('Hello, ' + name + '! How are you?');
+
+
+
+a = 10;
+b = 2;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
+
+
+let number1 = 30;
+let number2 = 20;
+console.log(number1 === number2); 
+
+
+
+let digit1 = 5;
+let digit2 = 18;
+let digit3 = 25;
+console.log((digit1+digit2+digit3)/3); 
+
+
+
+let digit = 12345;
+console.log(digit1 % 10); 
+
+
+
