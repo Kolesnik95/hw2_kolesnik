359d9d1d90607b99fce29953830ed45bfde14a13
Kolesnik95 <95kolesnik@gmail.com>
2023-07-31 21:03:15 +0300

    second commit

--- hw2.js
+++ hw2.js
@@ -3,31 +3,55 @@ let name = prompt('Як вас звати?');
 alert('Hello, ' + name + '! How are you?');
 
 
+let a = prompt('num1');
+let b = prompt('num2')
+a = parseInt(a);
+b = parseInt(b);
 
-a = 10;
-b = 2;
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
+console.log(a + b); 
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
 
 
 
-let number1 = 30;
-let number2 = 20;
+let number1 = prompt('num1');
+let number2 = prompt('num2');
 console.log(number1 === number2); 
 
 
 
-let digit1 = 5;
-let digit2 = 18;
-let digit3 = 25;
-console.log((digit1+digit2+digit3)/3); 
+let firstNumber = prompt('num1');
+let secondNumber = prompt('num2');
+let thirdNumber = prompt('num3');
+console.log((Number(firstNumber)+Number(secondNumber)+Number(thirdNumber))/3); 
 
 
 
-let digit = 12345;
-console.log(digit1 % 10); 
+let myNumber = prompt('number');  
+
+firstNumber = myNumber % 10;      
+console.log(firstNumber); 
+
+myNumber = (myNumber - firstNumber)/10;
+secondNumber = myNumber % 10; 
+console.log(secondNumber); 
+
+myNumber = (myNumber - secondNumber)/10;
+thirdNumber = myNumber % 10; 
+console.log(thirdNumber);
+
+myNumber = (myNumber - thirdNumber)/10;
+fourthNumber = myNumber % 10;
+console.log(fourthNumber);
+
+myNumber = (myNumber - fourthNumber)/10;
+fifthNumber = myNumber % 10;
+console.log(fifthNumber);
+
+console.log(fifthNumber, fourthNumber, thirdNumber, secondNumber, firstNumber);
+
+
 
 
 
